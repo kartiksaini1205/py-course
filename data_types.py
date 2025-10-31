a1ba40156da8d1eb31e68aace5b1c001299849aa
kartiksaini <kartiksaini0382@gmail.com>
2025-10-31 15:19:13 +0530

    initial commit

---
+++ data_types.py
@@ -0,0 +1,14 @@
+#single valued data types
+age = 30            # integer
+height = 5.9       # float
+is_std = True   # boolean
+complex_num = 3 + 4j  # complex number
+
+#multivalued data types
+str="Hello, World!"  # string
+li = [1, 2, 3, 4, 5]  # list
+tup = (10.0, 20.0)  # tuple
+dictionary = {'name': 'Kartik', 'age': 20}  # dictionary
+num = {1, 2, 3, 4}  # set
+
+print(age, height, is_std, complex_num, str, li, tup, dictionary, num)
---
+++ program1.py
@@ -0,0 +1,15 @@
+list1 = [1, 2, 3, "kartik", 5.5]
+tuple1 = (10, 20, "hello", 30.5)
+set1 = {1, 2, 3, 4, "world"}    
+dict1 = {'name': 'Kartik', 'age': 20, 'height': 5.9}
+
+print(list1[0: :1])
+print(list1[-1:-5:-1])
+print(list1[2])
+
+print(tuple1[1: :1])
+print(tuple1[-1:-4:-1])
+print(tuple1[2])   
+
+print(set1)
+print(dict1['name'])
